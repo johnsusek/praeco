b49345ba12e77dc491d9c5535eec6be1888f8be3
John Susek <john@johnsolo.net>
2019-01-17 17:00:13 -0600

    Add status page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Status from './views/Status.vue';
 import Errors from './views/Errors.vue';
 import Alerts from './views/Alerts.vue';
 import Silences from './views/Silences.vue';
@@ -22,6 +23,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/status',
+      name: 'status',
+      component: Status
     },
     {
       path: '/errors',
---
+++ src/views/Status.vue
@@ -0,0 +1,4 @@
+<template>
+    <!-- Used for app health checks to make sure the app has started -->
+    <div>SUCCESS</div>
+</template>
