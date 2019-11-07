19fcc8eae528e2685c7b62649c653fd0f04cf03d
John Susek <john@johnsolo.net>
2019-11-07 09:57:32 -0600

    Make the robots happy...

--- run_tests.sh
+++ run_tests.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
 # This text breaks on the linux build server because of canvas polyfill issues.
-if [[ $OSTYPE == darwin* ]];
-then
-./node_modules/.bin/vue-cli-service test:unit tests/unit/specs/alertText.spec.js &&
-./node_modules/.bin/vue-cli-service test:unit tests/unit/specs/ConfigAlert.spec.js &&
-./node_modules/.bin/vue-cli-service test:unit tests/unit/specs/ConfigCondition.spec.js &&
-./node_modules/.bin/vue-cli-service test:unit tests/unit/specs/ConfigYaml.spec.js &&
-./node_modules/.bin/vue-cli-service test:unit tests/unit/specs/ErrorsLog.spec.js &&
-./node_modules/.bin/vue-cli-service test:unit tests/unit/specs/luceneSyntaxBuilder.spec.js &&
-./node_modules/.bin/vue-cli-service test:unit tests/unit/specs/QueriesLog.spec.js &&
-./node_modules/.bin/vue-cli-service test:unit tests/unit/specs/RuleView.spec.js &&
-./node_modules/.bin/vue-cli-service test:unit tests/unit/specs/SilenceLog.spec.js
-else
 ./node_modules/.bin/vue-cli-service test:unit tests/unit/specs/alertText.spec.js &&
 ./node_modules/.bin/vue-cli-service test:unit tests/unit/specs/ConfigAlert.spec.js &&
 ./node_modules/.bin/vue-cli-service test:unit tests/unit/specs/ConfigYaml.spec.js &&
@@ -21,5 +9,4 @@ else
 ./node_modules/.bin/vue-cli-service test:unit tests/unit/specs/QueriesLog.spec.js &&
 ./node_modules/.bin/vue-cli-service test:unit tests/unit/specs/RuleView.spec.js &&
 ./node_modules/.bin/vue-cli-service test:unit tests/unit/specs/SilenceLog.spec.js
-fi
 
