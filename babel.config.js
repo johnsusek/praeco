97386a0986fb97d5f4fe54a90f22ea93329a37d8
nsano-rururu <nsano@ae.em-net.ne.jp>
2021-03-07 22:42:42 +0900

    fix unit test error

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   presets: ['@vue/cli-plugin-babel/preset'],
-  plugins: ['babel-plugin-istanbul']
+  plugins: [
+    'babel-plugin-istanbul',
+    '@babel/plugin-proposal-nullish-coalescing-operator'
+  ]
 };
