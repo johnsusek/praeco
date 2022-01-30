46fc886987051455e230dd8324c346b0261423ca
nsano-rururu <nsano@ae.em-net.ne.jp>
2022-01-31 02:09:15 +0900

    Add Exotel UnitTest

---
+++ tests/unit/mockData/alert/ruleDataExotelMessageBody.js
@@ -0,0 +1,33 @@
+export const ruleYaml = `__praeco_query_builder: '{"query":{"logicalOperator":"all","children":[]}}'
+alert:
+  - exotel
+alert_subject: this is a test subject
+alert_subject_args: []
+alert_text: this is a test body
+alert_text_args: []
+alert_text_type: alert_text_only
+doc_type: syslog
+exotel_account_sid: xxxxx1
+exotel_auth_token: xxxxx2
+exotel_from_number: xxxxx4
+exotel_to_number: xxxxx3
+exotel_message_body: message
+filter:
+  - query:
+      query_string:
+        query: '@timestamp:*'
+import: BaseRule.config
+index: hannibal-*
+is_enabled: false
+name: test123
+num_events: 10000
+realert:
+  minutes: 5
+timeframe:
+  minutes: 5
+timestamp_field: '@timestamp'
+timestamp_type: iso
+type: frequency
+use_count_query: true
+use_strftime_index: false
+`;
---
+++ tests/unit/specs/alert/ConfigYamlExotel.spec.js
@@ -0,0 +1,48 @@
+import { expect } from 'chai';
+import store from '@/store';
+import { mockAxios } from '../../setup';
+import { ruleYaml } from '../../mockData/alert/ruleDataExotel.js';
+
+mockAxios.onGet('/api/rules/test123').reply(200, { yaml: ruleYaml });
+
+describe('YAML parsing', () => {
+  it('renders the correct yaml', async () => {
+    await store.dispatch('config/load', { type: 'rules', path: 'test123' });
+
+    let yaml = store.getters['config/yaml']();
+
+    let expected = `__praeco_full_path: "test123"
+__praeco_query_builder: "{\\"query\\":{\\"logicalOperator\\":\\"all\\",\\"children\\":[]}}"
+alert:
+  - "exotel"
+doc_type: "syslog"
+exotel_account_sid: "xxxxx1"
+exotel_auth_token: "xxxxx2"
+exotel_from_number: "xxxxx4"
+exotel_to_number: "xxxxx3"
+filter:
+  - query:
+      query_string:
+        query: "@timestamp:*"
+generate_kibana_discover_url: false
+import: "BaseRule.config"
+index: "hannibal-*"
+is_enabled: false
+match_enhancements: []
+name: "test123"
+num_events: 10000
+realert:
+  minutes: 5
+terms_size: 50
+timeframe:
+  minutes: 5
+timestamp_field: "@timestamp"
+timestamp_type: "iso"
+type: "frequency"
+use_count_query: true
+use_strftime_index: false
+`;
+
+    return expect(yaml).to.equal(expected);
+  });
+});
