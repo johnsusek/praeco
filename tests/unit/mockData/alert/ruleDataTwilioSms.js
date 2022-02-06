24795a46efa31005bafd12f21744d595d93c95eb
nsano-rururu <nsano@ae.em-net.ne.jp>
2022-02-06 22:49:34 +0900

    Add Twilio UnitTest

---
+++ tests/unit/mockData/alert/ruleDataTwilioSms.js
@@ -0,0 +1,33 @@
+export const ruleYaml = `__praeco_query_builder: '{"query":{"logicalOperator":"all","children":[]}}'
+alert:
+  - twilio
+alert_subject: this is a test subject
+alert_subject_args: []
+alert_text: this is a test body
+alert_text_args: []
+alert_text_type: alert_text_only
+doc_type: syslog
+filter:
+  - query:
+      query_string:
+        query: '@timestamp:*'
+import: BaseRule.config
+index: hannibal-*
+is_enabled: false
+name: test123
+num_events: 10000
+realert:
+  minutes: 5
+timeframe:
+  minutes: 5
+timestamp_field: '@timestamp'
+timestamp_type: iso
+twilio_account_sid: ABCDEFGHIJKLMNOPQRSTUVWXYZ01234567
+twilio_auth_token: abcdefghijklmnopqrstuvwxyz012345
+twilio_from_number: '9876543210'
+twilio_to_number: '0123456789'
+twilio_use_copilot: false
+type: frequency
+use_count_query: true
+use_strftime_index: false
+`;
---
+++ tests/unit/specs/alert/ConfigYamlTwilioSms.spec.js
@@ -0,0 +1,49 @@
+import { expect } from 'chai';
+import store from '@/store';
+import { mockAxios } from '../../setup';
+import { ruleYaml } from '../../mockData/alert/ruleDataTwilioSms.js';
+
+mockAxios.onGet('/api/rules/test123').reply(200, { yaml: ruleYaml });
+
+describe('YAML parsing', () => {
+  it('renders the correct yaml', async () => {
+    await store.dispatch('config/load', { type: 'rules', path: 'test123' });
+
+    let yaml = store.getters['config/yaml']();
+
+    let expected = `__praeco_full_path: "test123"
+__praeco_query_builder: "{\\"query\\":{\\"logicalOperator\\":\\"all\\",\\"children\\":[]}}"
+alert:
+  - "twilio"
+doc_type: "syslog"
+filter:
+  - query:
+      query_string:
+        query: "@timestamp:*"
+generate_kibana_discover_url: false
+import: "BaseRule.config"
+index: "hannibal-*"
+is_enabled: false
+match_enhancements: []
+name: "test123"
+num_events: 10000
+realert:
+  minutes: 5
+terms_size: 50
+timeframe:
+  minutes: 5
+timestamp_field: "@timestamp"
+timestamp_type: "iso"
+twilio_account_sid: "ABCDEFGHIJKLMNOPQRSTUVWXYZ01234567"
+twilio_auth_token: "abcdefghijklmnopqrstuvwxyz012345"
+twilio_from_number: "9876543210"
+twilio_to_number: "0123456789"
+twilio_use_copilot: false
+type: "frequency"
+use_count_query: true
+use_strftime_index: false
+`;
+
+    return expect(yaml).to.equal(expected);
+  });
+});
