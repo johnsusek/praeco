e68fe440db9409977c483f76bc3c73da6a3d3f6e
Naoyuki Sano <nsano@ae.em-net.ne.jp>
2026-09-21 23:36:33 +0900

    fix js-yaml import

--- src/store/configs.js
+++ src/store/configs.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import yaml from 'js-yaml';
+import * as yaml from 'js-yaml';
 import cloneDeep from 'lodash.clonedeep';
 // TODO: error  Dependency cycle via @/store:5  import/no-cycle
 import { logger } from '@/lib/logger.js';
